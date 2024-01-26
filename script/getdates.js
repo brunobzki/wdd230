3900cc7df5d2d53b48ebbf3c15aeaab4979577e3
brunobzki <bpanzacchi@gmail.com>
2024-01-26 00:30:49 -0300

    week3

--- script/getdates.js
+++ script/getdates.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let lastModifiedParagraph = document.getElementById("lastModified");
     lastModifiedParagraph.innerHTML = "Última modificación: " + document.lastModified;
 });
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
