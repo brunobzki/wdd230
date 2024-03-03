7f5fb982d943cf81e7b6ae7a6d34d14d8d57e91b
brunobzki <bpanzacchi@gmail.com>
2024-03-02 23:47:07 -0300

    zdvasdaf

--- chamber/script/getdates.js
+++ chamber/script/getdates.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   let currentYear = new Date().getFullYear();
   let firstParagraph = document.getElementById("footerFirstParagraph");
-
   let decoratedName = "Bruno Panzacchi";
-
   let uruguayFlagImage = `<img src="images/Flag.png" alt="Flag of Uruguay" width="95" height="75"style="vertical-align: middle;">`;
-
+  var timestampField = document.getElementById('timestamp');
   firstParagraph.innerHTML = `&copy; ${currentYear} ${decoratedName}`;
 
   let lastModifiedParagraph = document.getElementById("lastModified");
   lastModifiedParagraph.innerHTML = "Last modification: " + document.lastModified;
+  timestampField.value = Date.now();
 });
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
@@ -23,6 +22,7 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("#content");
 const menuButton = document.querySelector("#menu");
 const infobackground = document.querySelector(".data")
+const joinbackground = document.querySelector("div")
 
 modeButton.addEventListener("click", () => {
   if (modeButton.textContent.includes("🕶️")) {
@@ -30,12 +30,17 @@ modeButton.addEventListener("click", () => {
     main.style.color = "#fff";
     menuButton.style.color = "#fff";
     modeButton.textContent = "🔆";
-    infobackground.style.background = "#000"
+    infobackground.style.background = "#000";
+    joinbackground.style.background = "#000";
+    joinbackground.style.color = "#fff";
   } else {
     main.style.background = "#eee";
     main.style.color = "#000";
     menuButton.style.color = "#000";
     modeButton.textContent = "🕶️";
     infobackground.style.background = "#a9a9a952"
+    joinbackground.style.background = "#fff";
+    joinbackground.style.color = "#000"
+
   }
 });
---
+++ chamber/script/submit.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+	
+	function handleSubmit(event) {
+		
+		event.preventDefault();
+
+	
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("#email");
+	form.addEventListener("submit", handleSubmit);
+});
